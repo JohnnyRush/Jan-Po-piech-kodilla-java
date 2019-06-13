90fc5fee05e44711b4b8ab87c7a797a80a08666d
JohnnyRush <nasz_email_do_github>
2019-06-14 01:00:48 +0200

    submoduł 6.2

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int add(int a, int b) {
+        int c;
+        c = a + b;
+        return c;
+    }
+    public int subtract(int a, int b) {
+        int c;
+        c = a - b;
+        return c;
+    }
+}
+
