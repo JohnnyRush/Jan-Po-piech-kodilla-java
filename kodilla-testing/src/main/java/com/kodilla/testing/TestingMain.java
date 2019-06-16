b3bf64fd0cd9310a9bbbf1b135a9e050bf230a0a
JohnnyRush <nasz_email_do_github>
2019-06-16 23:26:53 +0200

    submoduł 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,31 +4,5 @@ import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
-    public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-        Calculator calc = new Calculator();
-
-        if(calc.add(9,6) == 17 ) {
-            System.out.println("well done");
-        }else {
-            System.out.println("failure");
-        }
-
-        if(calc.subtract(10, 5) == 5){
-            System.out.println("well done");
-        }else {
-            System.out.println("failure");
-        }
-
-    }
-
-
+    public static void main(String[] args) {}
 }
