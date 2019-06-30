2f89278ece8b732a0e5eb89bb8bf7401f5d042b8
JohnnyRush <nasz_email_do_github>
2019-06-30 13:17:22 +0200

    Task 8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/Flight.java
@@ -0,0 +1,19 @@
+package com.kodilla.exception.test;
+
+public class Flight {
+    final private String departureAirport;
+    final private String arrivalAirport;
+
+    public Flight(String departureAirport, String arrivalAirport) {
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SkyScanner.java
@@ -0,0 +1,43 @@
+package com.kodilla.exception.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SkyScanner {
+
+    public void findFlight(Flight flight) throws RouteNotFoundException{
+        Map<String, Boolean> fligthList = new HashMap<>();
+        fligthList.put("Berlin", false);
+        fligthList.put("Moscow", true);
+        fligthList.put("Paris", true);
+        fligthList.put("London", false);
+
+        List<String> list = new ArrayList<>(fligthList.keySet());
+        if(list.contains(flight.getArrivalAirport())){
+                if(fligthList.get(flight.getArrivalAirport()) == true){
+                    System.out.println("Flight found!");
+                }else{
+                    System.out.println("You can not fly there!");
+                }
+        }else{
+            throw new RouteNotFoundException("Route not found.");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Flight flight1 = new Flight("Berlin" , "Sydney");
+        SkyScanner skyScanner = new SkyScanner();
+
+        try{
+            skyScanner.findFlight(flight1);
+        }catch (RouteNotFoundException e){
+            System.out.println("Error: " + e);
+        }finally {
+            System.out.println("Search completed.");
+        }
+
+    }
+}
