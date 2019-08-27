60207f02ab6fb64416744978e0297a6c7bdb8701
JohnnyRush <nasz_email_do_github>
2019-08-28 01:19:13 +0200

    Array with task list

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,19 +1,41 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class BoardConfig {
-    @Bean
-    public TaskList createToDoList(){ return new TaskList();}
-    @Bean
-    public TaskList createInProgressList(){ return new TaskList();}
-    @Bean
-    public TaskList createDoneList(){ return new TaskList();}
+
+    @Autowired
+    @Qualifier("taskList1")
+    TaskList taskList1;
+    @Autowired
+    @Qualifier("taskList2")
+    TaskList taskList2;
+    @Autowired
+    @Qualifier("taskList3")
+    TaskList taskList3;
+
+    @Bean(name = "taskList1")
+    public TaskList getToDoList() {
+        return new TaskList();
+    }
+
+    @Bean(name = "taskList2")
+    public TaskList getInProgressList() {
+        return new TaskList();
+    }
+
+    @Bean(name = "taskList3")
+    public TaskList getDoneList() {
+        return new TaskList();
+    }
 
     @Bean
     public Board board() {
-        return new Board(createToDoList(), createInProgressList(), createDoneList());
+        return new Board(taskList1, taskList2, taskList3);
     }
 }
