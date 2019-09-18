c0e083b72b3643149871721e363739b04b2666e9
JohnnyRush <nasz_email_do_github>
2019-09-18 10:57:39 +0200

    task

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,39 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+
+@RunWith(SpringRunner.class)
+@Transactional
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String LISTNAME = "ToDo";
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @Test
+    public void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+
+        //When
+        taskListDao.save(taskList);
+
+        //Then
+        String lN = taskList.getListName();
+        TaskList readTaskList = taskListDao.findByListName(lN);
+        Assert.assertTrue(readTaskList.equals(taskList));
+
+    }
+}
