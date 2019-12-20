45b0d041c3c3d36f89c960b1360c636a19e3242e
JohnnyRush <pospiechjan@gmail.com>
2019-12-20 20:19:14 +0100

    adapter

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -9,10 +9,10 @@ import java.util.*;
 public class MedianAdapter extends MedianAdaptee implements Clasifier {
 
     @Override
-    public int publicationYearMedian(Set<Book> bookSet) {
+    public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> bookSet) {
         Map<BookSignature, com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book> books = new HashMap<>();
 
-        for (Book book : bookSet) {
+        for (com.kodilla.patterns2.adapter.bookclasifier.librarya.Book book : bookSet) {
             books.put(new BookSignature(book.getSignature()), new com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book(book.getAuthor(), book.getTitle(), book.getPublicationYear()));
         }
         return medianPublicationYear(books);
